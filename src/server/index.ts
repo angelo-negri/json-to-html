6363be21627b90bdc0e8b8612a221cc8bab3f02e
Angelo <158775668+angelo-negri@users.noreply.github.com>
2024-05-14 14:21:48 +0300

    feat: Update the main server route to render the react component

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,29 @@
+import React from "react";
 import express from "express";
+import ReactDOMServer from "react-dom/server";
+import App from "../client/App/index";
 
 const server = express();
 
+server.use("/static", express.static("dist/static"));
+
 server.get("/", (_, res) => {
-  res.send("Hello from Server");
+  const app = ReactDOMServer.renderToString(React.createElement(App));
+
+  const html = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>My SSR App</title>
+    </head>
+    <body>
+      <div id="root">${app}</div>
+      <script src="/static/client.js"></script>
+    </body>
+    </html>
+  `;
+
+  res.send(html);
 });
 
 server.listen(3000, () => {
