5fb0ca9d49ad36d04b35a85a182d9fcd24b6f7cf
Angelo <158775668+angelo-negri@users.noreply.github.com>
2024-05-14 16:42:50 +0300

    feat: Fetch the API data

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { hydrateRoot } from "react-dom/client";
-import App from "./App/index";
+import App from "./App";
 
 const domNode = document.getElementById("root");
 if (domNode) {
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,29 +1,45 @@
 import React from "react";
 import express from "express";
 import ReactDOMServer from "react-dom/server";
-import App from "../client/App/index";
+import App from "../client/App";
+import { JsonDesignSet } from "../client/types";
 
+require("dotenv").config();
 const server = express();
 
 server.use("/static", express.static("dist/static"));
 
-server.get("/", (_, res) => {
-  const app = ReactDOMServer.renderToString(React.createElement(App));
+server.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const apiUrl = `${process.env.API_URL}/${id}/json`;
+    const response = await fetch(apiUrl);
+    const data: JsonDesignSet = await response.json();
 
-  const html = `
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>My SSR App</title>
-    </head>
-    <body>
-      <div id="root">${app}</div>
-      <script src="/static/client.js"></script>
-    </body>
-    </html>
-  `;
+    const app = ReactDOMServer.renderToString(React.createElement(App));
 
-  res.send(html);
+    const html = `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>JSON to React SSR</title>
+      </head>
+      <body>
+        <div id="root">${app}</div>
+        <script src="/static/bundle.js"></script>
+      </body>
+      </html>
+    `;
+
+    res.send(html);
+  } catch (error) {
+    console.error("Failed to fetch API data:", error);
+    res.status(500).send("Error fetching API data");
+  }
+});
+
+server.get("*", (req, res) => {
+  res.status(404).send("Page not found - try adding an ID to the URL");
 });
 
 server.listen(3000, () => {
